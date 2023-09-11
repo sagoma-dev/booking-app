385817e05174267acb14525fea48068164dc4f89
Samuel Gómez <sagomadev@gmail.com>
2023-09-11 18:00:53 +0200

    Add server data, landing page and router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Route, Switch } from "wouter";
+import HotelList from "./components/HotelList";
+
+const client = new QueryClient();
+
 function App() {
-  return <h1>Booking App</h1>;
+  return (
+    <QueryClientProvider client={client}>
+      <Switch>
+        <Route path="/" component={HotelList} />
+      </Switch>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
